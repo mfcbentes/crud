4dde6f466496f57ab91f50ce40fd4abc9d6ffadf
Manoel Fernando C. Bentes <79587445+mfcbentes@users.noreply.github.com>
2022-04-06 23:37:57 -0300

    Update Connection.class.php

--- db/Connection.class.php
+++ db/Connection.class.php
@@ -8,7 +8,7 @@ class Connection
     {
 
         if (!isset(self::$instance)) {
-            self::$instance = new PDO("mysql:host=localhost;dbname=agendamentos", "mfcbentes", "mfcb@2018.stm", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            self::$instance = new PDO("mysql:host=localhost;dbname=agendamentos", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
 
